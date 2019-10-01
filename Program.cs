ec0ad192e51032bd43e17a5bad661f216022a29a
YuryPetruchenia <yourockpet@gmail.com>
2019-10-01 21:41:39 +0300

    add

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Practis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[6];
+
+            array[0] = Convert.ToInt32(Console.ReadLine());
+            array[1] = Convert.ToInt32(Console.ReadLine());
+            array[2] = Convert.ToInt32(Console.ReadLine());
+            array[3] = Convert.ToInt32(Console.ReadLine());
+            array[4] = Convert.ToInt32(Console.ReadLine());
+            array[5] = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+
+            Console.WriteLine();
+
+            array[2] = array[2] * 10;
+
+            Console.WriteLine("Измененный массив:");
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+
+        }
+    }
+}
