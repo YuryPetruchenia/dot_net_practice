297d7f10029ddc4ff30ae28f8d52b5f2a44699f1
YuryPetruchenia <yourockpet@gmail.com>
2019-10-03 19:33:40 +0300

    Create Task1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            void CrArray()
+            {
+                int[] array = new int[3];
+
+            }
+        }
+    }
+}
