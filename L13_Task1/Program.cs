8414a0d1a0c277b4b49e1617b7f01ebdd880b26d
YuryPetruchenia <yourockpet@gmail.com>
2019-10-24 20:28:55 +0300

    Add task1

---
+++ L13_Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace L13_Task1
+{
+    class Program
+    {
+                
+        static void Main(string[] args)
+        {
+            var song = new
+            {
+                name = "david bowie",
+                length = 200,
+                autor = "david bowie",
+                date = 1960
+            };
+            void GetSongData()
+            {
+                Console.WriteLine($"song: {song.name} length {song.length} date {song.date}");
+                
+            }
+            GetSongData();
+        }
+    }
+}
---
+++ L13_Task1/Song.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace L13_Task1
+{
+    class Song
+    {
+        private string _name;
+        private int _length;
+        private string _autor;
+        private DateTime _date;
+
+        public string Name { get { return _name; } set { Name = _name; } }
+        public int Length { get { return _length; } set { Length = _length; } }
+        public string Autor { get { return _autor; } set { Autor = _autor; } }
+        public DateTime Date { get { return _date; } set { Date = _date; } }
+
+
+    }
+}
